c68acd79c517b684b22baa96d5258919eaf3ae7d
nteller8 <nteller@udesa.edu.ar>
2022-11-15 14:52:12 -0300

    cambios

--- java/buscador.js
+++ java/buscador.js
@@ -1,3 +1,24 @@
 let buscador = document.querySelector (".buscador");
 let formulario = document.querySelector ("form");
 let campoAEvaluar = document.querySelector ("[name='q']")
+
+
+function buscarYMostrarFavoritos (id){
+    //fetch
+    let url = `https://rickandmortyapi.com/api/character/${id}`
+
+    fetch (url)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data);
+            lista.innerHTML += `<article>
+                            <img src="${data.image}">
+                            <p> Nombre: ${data.name}</p>
+                            
+                            </article>`
+        })
+        .catch(function(e){
+            console.log(e);
+        })
