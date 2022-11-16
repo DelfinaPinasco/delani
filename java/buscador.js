b15e98bef0dd0fdf2b7a0c763fd33ad5b2efd794
laramanfredini <laramanfredinii@gmail.com>
2022-11-16 15:46:14 -0300

    cambios

--- java/buscador.js
+++ java/buscador.js
@@ -1,3 +1,6 @@
-let buscador = document.querySelector (".buscador");
+/* let buscador = document.querySelector (".buscador");
 let formulario = document.querySelector ("form");
-let campoAEvaluar = document.querySelector ("[name='q']")
+let campoAEvaluar = document.querySelector ("[name='q']") */
+
+let url ('https://api.themoviedb.org/3/authentication/token/new?api_key=15370bef1a25ea674deaaf70270ad202')
+fetch
