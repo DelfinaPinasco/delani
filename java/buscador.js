59fc59fbb6d996c4e4141674f54f867c63c65d70
nteller8 <nteller@udesa.edu.ar>
2022-11-16 15:55:24 -0300

    intentando

--- java/buscador.js
+++ java/buscador.js
@@ -1,25 +1,30 @@
-let buscador = document.querySelector (".buscador");
-let formulario = document.querySelector ("form");
-let campoAEvaluar = document.querySelector ("[name='q']")
+
+let searchbar=document.getElementById('searchbar')
 
 
-function buscarYMostrarFavoritos (id){
+let url = `https://api.themoviedb.org/3/movie/550?api_key=15370bef1a25ea674deaaf70270ad202${id}`
+fetch (url)
+function buscar (id){
     //fetch
-    let url = `https://rickandmortyapi.com/api/character/${id}`
+    let url = `https://api.themoviedb.org/3/movie/550?api_key=15370bef1a25ea674deaaf70270ad202${id}`
 
     fetch (url)
         .then(function(response){
             return response.json();
         })
         .then(function(data){
-            console.log(data);
-            lista.innerHTML += `<article>
-                            <img src="${data.image}">
-                            <p> Nombre: ${data.name}</p>
-                            
-                            </article>`
+            console.log(datos);
+            let datos = document.querySelector(".peliculas")
+            let resultados =datos;
+            for
         })
         .catch (function(e){
             console.log(e);
         })
     }
+
+
+
+let queryString = location.search; //Obtengo la QS
+let queryStringToObject = new URLSearchParams(queryString); //La trasnformo en OL
+let id = queryStringToObject.get('id'); //Obtengo los datos de una propiedad con get()
