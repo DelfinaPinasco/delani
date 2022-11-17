df07a912123f66dcf7606314be2fb8121d094654
nteller8 <nteller@udesa.edu.ar>
2022-11-17 18:52:23 -0300

    cambios x nina

--- java/index.js
+++ java/index.js
@@ -1,29 +1,87 @@
-let url = 'https://api.themoviedb.org/3/movie/550?api_key=15370bef1a25ea674deaaf70270ad202'
 
-/*  cambiar la data
-fetch(url)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        console.log(data)
-        let info = data.results
-        let container = document.querySelector('.characterList');
-        let characters = '';
+/* para que aparezcan la division por section, cada una con su url y selector */
+let apiKey = "15370bef1a25ea674deaaf70270ad202a"
 
-        for(let i=0; i<info.length; i++){
-            characters += `<article>
-                                <img src=${info[i].image} alt='' />
-                                <a href='detalle.html?id=${info[i].id}'><p>Name: ${info[i].name}</p></a>
-                                <p>Status: ${info[i].status} </p>
-                            </article>`
-        }
-        
-        container.innerHTML = characters;
+let priurl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`
+let prisection= document.querySelector(".peliculaspopulares")
 
-        
-    })
-    .catch(function(error){
-        console.log(error);
-    })
-    */
+let segurl = `https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}&language=en-US&page=1`
+let segsection= document.querySelector(".seriespopulares")
+
+let terurl = `https://api.themoviedb.org/3/movie/latest?api_key=${apiKey}&language=en-US`
+let tersection= document.querySelector(".peliculasnuevas")
+
+/*cada section tiene un url y un fetch propio*/
+
+
+//prisection
+
+fetch(priurl)
+.then(function(respuesta) {
+    return respuesta.json();
+})
+.then(function(data) {
+    console.log(data);
+    let okey = ""
+    for (let index = 0; index < 5; index++) {
+        const element = data[index];
+        okey+=`<article class= cartelera>
+        <img src="https://image.tmdb.org/t/p/w500${element.poster_path}" alt='imagen' />
+         <p>Título:${element.title}</p>
+         <p>Fecha de estreno: ${element.release_date} </p>
+        </article>`
+    }
+    prisection.innerHTML = okey
+    return data
+})
+.catch(function(error){
+    console.log('El error es' + error);
+    return error
+})
+    
+//segsection
+
+fetch(segurl)
+.then(function(respuesta) {
+    return respuesta.json();
+})
+.then(function(data) {
+    console.log(data);
+    let okey = ""
+    for (let index = 0; index < 5; index++) {
+        const element = data[index];
+        okey+=`<article class= cartelera>
+        <img src="https://image.tmdb.org/t/p/w500${element.poster_path}" alt='imagen' />
+        <p>Título:${element.title}</p>
+        <p>Fecha de estreno: ${element.release_date} </p>
+       </article>`
+    }
+    section1.innerHTML = okey
+})
+.catch(function(error){
+    console.log('El error es' + error);
+    return error
+})
+
+//tersection
+fetch(terurl)
+.then(function(respuesta) {
+    return respuesta.json();
+})
+.then(function(data) {
+    console.log(data);
+    let okey = ""
+    for (let index = 0; index < 5; index++) {
+        const element = data[index];
+        okey+=`<article class= cartelera>
+        <img src="https://image.tmdb.org/t/p/w500${element.poster_path}" alt='imagen' />
+         <p>Título:${element.title}</p>
+         <p>Fecha de estreno: ${element.release_date} </p>
+        </article>`
+    }
+    tersection.innerHTML = okey
+})
+.catch(function(error){
+    console.log('El error es' + error);
+    return error
+})
---
+++ java/search-results.js
@@ -0,0 +1,35 @@
+/* direccionarlo con el endpoint de searchresults,  esto es lo de rick & morty */
+
+let query = location.search; // recuperamos el querystring
+let stringToObject = new URLSearchParams(query); //La trasnformo en OL, // usando el querystring, creamos un objeto del tipo URLSearchParams
+let aBuscar = stringToObject.get('.search'); //Obtengo los datos de una propiedad con get()
+let url = `https://api.themoviedb.org/3/search/company?api_key=${aBuscar}&page=1`
+
+console.log(url);
+
+fetch(url)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+        let info = data.results
+        let container = document.querySelector('.sectionnn');
+        let characters = '';
+
+        for(let i=0; i<info.length; i++){  /* i++ --> para que se itere */
+            characters += `<article>
+                                <img src=${info[i].image} alt='' />
+                                <a href='detalle.html?id=${info[i].id}'><p>Name: ${info[i].name}</p></a>
+                                <p>Status: ${info[i].status} </p>
+                                
+                            </article>`
+        }
+        container.innerHTML = characters;
+
+
+    })
+
+    .catch(function(error){
+        console.log(error);
+    })
