64bfd18c344b79c79fcee7f4650502a61da15371
laramanfredini <laramanfredinii@gmail.com>
2022-11-18 15:06:24 -0300

    cambios

--- java/genres.js
+++ java/genres.js
@@ -20,13 +20,25 @@ fetch ("https://api.themoviedb.org/3/movie/{movie_id}?api_key=15370bef1a25ea674d
         
 })
 .catch (error => console.log(error))
-let sectionseries = document.querySelector('.section-series')
+let sectionseries = 
+document.querySelector('.section-series')
+
 fetch ("https://api.themoviedb.org/3/movie/{movie_id}?api_key=15370bef1a25ea674deaaf70270ad202&language=en-US"
 .then(function(res) {
     return res.json();
 })
-
-.catch(function(error){
-    console.log('El error es' + error);
-    return error
+.then(function(data) {
+    console.log(data);
+    let generosDos = data;
+    for(let i=0; i<generosDos.length; i++){
+        `
+        generoSerie.innerHTML += 
+							
+<ul>
+<li>
+<a href="detail-genres.html?id=${generos[i].id}">${generos[i].name}</a>
+</li>
+</ul>`    }
+        
 })
+.catch(error => console.log(error))
