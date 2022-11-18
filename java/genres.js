40dfa5e6de3f094b554a781b8e3056ee6b95ad9d
nteller8 <nteller@udesa.edu.ar>
2022-11-18 18:33:09 -0300

    cambios

--- java/genres.js
+++ java/genres.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-
-let apikey = "15370bef1a25ea674deaaf70270ad202"
-let url = `https://api.themoviedb.org/3/movie/{movie_id}?api_key=15370bef1a25ea674deaaf70270ad202&language=en-US`
-let generoPelicula = document.querySelector('.section-movies')
-
-fetch (url)
-=======
 let generoPelicula = document.querySelector('.section-movies')
 
 fetch ("https://api.themoviedb.org/3/movie/{movie_id}?api_key=15370bef1a25ea674deaaf70270ad202&language=en-US"
@@ -13,22 +5,11 @@ fetch ("https://api.themoviedb.org/3/movie/{movie_id}?api_key=15370bef1a25ea674d
 .then(function(res) {
     return res.json();
 })
->>>>>>> 4d58290fc3ec549c2db31b6eaec6303725c891e2
 .then(function(data) {
     console.log(data);
         let generos = data;
 
     for(let i=0; i<generos.length; i++){
-<<<<<<< HEAD
-		generoPelicula.innerHTML += `
-							
-    <ul>
-    <li>
-    <a href="detail-genres.html?id=${generos[i].id}">${generos[i].name}</a>
-    </li>
-    </ul>`
-        }
-=======
 	        	generoPelicula.innerHTML += `
 				
                 
@@ -42,7 +23,6 @@ fetch ("https://api.themoviedb.org/3/movie/{movie_id}?api_key=15370bef1a25ea674d
 
 </ul>`
     }
->>>>>>> 4d58290fc3ec549c2db31b6eaec6303725c891e2
         
 })
 .catch (error => console.log(error))
@@ -57,7 +37,7 @@ fetch (url)
 .then(function(data) {
     console.log(data);
         let generosDos = data;
-    for(let i=0; i<generosDos.length; i++){
+    for(let i=0; i<generosDos.length; i++) {
         `       generoSerie.innerHTML += `
 							
 <ul>
@@ -67,14 +47,7 @@ fetch (url)
 <a href="detail-genres.html?id=${generosDos[i].id}">${generosDos[i].name}</a>
 
 </li>
-<<<<<<< HEAD
 </ul>`    
 }
-=======
-
-</ul>`    
-    }
->>>>>>> 4d58290fc3ec549c2db31b6eaec6303725c891e2
-        
-
-})
+)        
+    })
