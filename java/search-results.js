fac3c3f52d47b732f48f7cd4d3f671de45297824
nteller8 <nteller@udesa.edu.ar>
2022-11-18 11:38:14 -0300

    cambios BIEN

--- java/search-results.js
+++ java/search-results.js
@@ -3,7 +3,7 @@
 let query = location.search; // recuperamos el querystring
 let stringToObject = new URLSearchParams(query); //La trasnformo en OL, // usando el querystring, creamos un objeto del tipo URLSearchParams
 let aBuscar = stringToObject.get('.search'); //Obtengo los datos de una propiedad con get()
-let url = `https://api.themoviedb.org/3/search/company?api_key=${aBuscar}&page=1`
+let url =`https://api.themoviedb.org/3/search/movie?api_key=15370bef1a25ea674deaaf70270ad202&language=en-US&query=${aBuscar}&page=1&include_adult=false`
 
 console.log(url);
 
