bf3541f538565b8e5ead4e46a5207e55516ebd3c
nteller8 <nteller@udesa.edu.ar>
2022-11-18 18:26:05 -0300

    cambios

--- java/detail-movies.js
+++ java/detail-movies.js
@@ -36,3 +36,46 @@ fetch (url)
     console.log('El error es' + error);
     return error
 }))
+
+
+/*let query1 = location.search
+console.log (query1);
+let query2 = new URLSearchParams(query1);
+console.log(query2)
+let id = query2.get("id");
+console.log(id); 
+
+
+//let url = `https://api.themoviedb.org/3/discover/movie?api_key=15370bef1a25ea674deaaf70270ad202&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${id}&with_watch_monetization_types=flatrate`
+let url =  `https://api.themoviedb.org/3/discover/movie?api_key=385115c8e9bd0bc996d46c69d38601de&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${id}&with_watch_monetization_types=flatrate`
+
+fetch(url)
+.then(function(respuesta) {
+    return respuesta.json();
+})
+
+.then(function(data) {
+    console.log(data);
+   <!-- let info = data.results     /* el results es generico y contiene el array  */
+    let genero = document.querySelector(".section-movies")
+    let contenido = "";       /* comienza vacía porque es la lista a la que se le agregan los elementos  */
+
+    for (let index = 0; index < 5; index++) {         /* i++ se va iterando */ 
+    contenido+= `<article class= ".section-movies article">
+    <a href ="./detail-genres.html?id=${info[index].id}">    
+    <div>
+        <h3>${info[i].id} </h3>            
+        <img src="https://image.tmdb.org/t/p/w500${info[index].poster_path}" alt='' />
+        <h3>${info[index].title}</h3>
+        <h3>Fecha de estreno: ${info[index].release_date} </h3>
+        </a>
+        <div/>
+    </article>`
+}
+    genero.innerHTML += contenido
+
+})
+
+    .catch(function(error){
+        console.log(error)
+}) */ 
--- java/genres.js
+++ java/genres.js
@@ -1,29 +1,28 @@
 
 let apikey = "15370bef1a25ea674deaaf70270ad202"
-let url = "https://api.themoviedb.org/3/movie/{movie_id}?api_key=15370bef1a25ea674deaaf70270ad202&language=en-US"
+let url = `https://api.themoviedb.org/3/movie/{movie_id}?api_key=15370bef1a25ea674deaaf70270ad202&language=en-US`
 let generoPelicula = document.querySelector('.section-movies')
 
-fetch ("https://api.themoviedb.org/3/movie/{movie_id}?api_key=15370bef1a25ea674deaaf70270ad202&language=en-US"
-)
+fetch (url)
 .then(function(data) {
     console.log(data);
     let generos = data;
     for(let i=0; i<generos.length; i++){
 		generoPelicula.innerHTML += `
 							
-<ul>
-<li>
-<a href="detail-genres.html?id=${generos[i].id}">${generos[i].name}</a>
-</li>
-</ul>`
-    }
+    <ul>
+    <li>
+    <a href="detail-genres.html?id=${generos[i].id}">${generos[i].name}</a>
+    </li>
+    </ul>`
+        }
         
 })
 .catch (error => console.log(error))
 let sectionseries = 
 document.querySelector('.section-series')
 
-fetch ("https://api.themoviedb.org/3/movie/{movie_id}?api_key=15370bef1a25ea674deaaf70270ad202&language=en-US"
+fetch (url)
 .then(function(res) {
     return res.json();
 })
@@ -38,7 +37,8 @@ fetch ("https://api.themoviedb.org/3/movie/{movie_id}?api_key=15370bef1a25ea674d
 <li>
 <a href="detail-genres.html?id=${generos[i].id}">${generos[i].name}</a>
 </li>
-</ul>`    }
+</ul>`    
+}
         
+
 })
-.catch(error => console.log(error))
