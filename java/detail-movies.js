1dcb0e8fc5b0cd3634aafa10c7291bd15dd46d56
DelfinaPinasco <pinascod@udesa.edu.ar>
2022-11-18 12:30:38 -0300

    cambios

--- java/detail-movies.js
+++ java/detail-movies.js
@@ -16,7 +16,8 @@ for (let index = 0; index < info.length; index++) /* i++ se va iterando */
     `<img class = "fotofast" src= "https://image.tmdb.org/t/p/w500${info[index].poster_path}" alt='' /> 
     <h1>${info[index].title}</h1>
     <p class = "estreno"> Fecha de estreno: ${info[index].release_date} </p>
-    <p class = "calificacion" > ${info[index].vote_avarage} </p>
+    <p class = "calificacion" > Calificación: ${info[index].vote_avarage} </p>
+    
 `
 
 
