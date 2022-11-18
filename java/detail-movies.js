73abbfddce62d37880c0cb8e48831cf65446694f
DelfinaPinasco <pinascod@udesa.edu.ar>
2022-11-18 15:33:55 -0300

    cambios

--- java/detail-movies.js
+++ java/detail-movies.js
@@ -1,19 +1,25 @@
+window.addEventListener("load", function(){
+    let moviedetail = location.search;
+    let movieobjeto = new URLSearchParams(moviedetail);
+    let movie_id = movieobjeto.get('id');
+})
+
 let apikey = "15370bef1a25ea674deaaf70270ad202"
-let url = "https://api.themoviedb.org/3/movie/{movie_id}?api_key=15370bef1a25ea674deaaf70270ad202&language=en-US"
+let url = `(https://api.themoviedb.org/3/movie/${movie_id}?${api_key}=15370bef1a25ea674deaaf70270ad202&language=en-US")`
 
 fetch (url)
     .then(function(res) {
     return res.json();
     })
     .then(function(data) { 
-    console.log(data);// hay que seguir con esto
     let info = data.results
-    let contenido = "";
+    console.log(data);// hay que seguir con esto
+    
+
+        let container = document.querySelector('.') //aca va la class a la que queremos entrar
 
 
-for (let index = 0; index < info.length; index++) /* i++ se va iterando */ 
-    contenido+= 
-    `<img class = "fotofast" src= "https://image.tmdb.org/t/p/w500${info[index].poster_path}" alt='' /> 
+    contenido+=  `<img class = "fotofast" src= "https://image.tmdb.org/t/p/w500${info[index].poster_path}" alt='' /> 
     <h1>${info[index].title}</h1>
     <p class = "estreno"> Fecha de estreno: ${info[index].release_date} </p>
     <p class = "calificacion" > Calificación: ${info[index].vote_avarage} </p>
