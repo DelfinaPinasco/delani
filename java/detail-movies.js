dc59423efecf7dccf9b6b201ac3126e0022bf50b
nteller8 <nteller@udesa.edu.ar>
2022-11-18 13:50:30 -0300

    cambios

--- java/detail-movies.js
+++ java/detail-movies.js
@@ -6,7 +6,9 @@ let id = query2.get("id");
 console.log(id);
 
 let apikey = "15370bef1a25ea674deaaf70270ad202"
-let url = "https://api.themoviedb.org/3/movie/{movie_id}?api_key=15370bef1a25ea674deaaf70270ad202&language=en-US"
+let url = `https://api.themoviedb.org/3/movie/{movie_id}?api_key=${apikey}&language=en-US
+
+`
 
 fetch (url)
 .then(function(res) {
