d9adbcf73034d4ce17a1b6d089e6d0ea0160e411
laramanfredini <laramanfredinii@gmail.com>
2022-11-18 18:32:47 -0300

    agregar

--- java/genres.js
+++ java/genres.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-
-let apikey = "15370bef1a25ea674deaaf70270ad202"
-let url = `https://api.themoviedb.org/3/movie/{movie_id}?api_key=15370bef1a25ea674deaaf70270ad202&language=en-US`
-let generoPelicula = document.querySelector('.section-movies')
-
-fetch (url)
-=======
 let generoPelicula = document.querySelector('.section-movies')
 
 fetch ("https://api.themoviedb.org/3/movie/{movie_id}?api_key=15370bef1a25ea674deaaf70270ad202&language=en-US"
@@ -13,22 +5,11 @@ fetch ("https://api.themoviedb.org/3/movie/{movie_id}?api_key=15370bef1a25ea674d
 .then(function(res) {
     return res.json();
 })
->>>>>>> 4d58290fc3ec549c2db31b6eaec6303725c891e2
 .then(function(data) {
     console.log(data);
         let generos = data;
 
     for(let i=0; i<generos.length; i++){
-<<<<<<< HEAD
-		generoPelicula.innerHTML += `
-							
-    <ul>
-    <li>
-    <a href="detail-genres.html?id=${generos[i].id}">${generos[i].name}</a>
-    </li>
-    </ul>`
-        }
-=======
 	        	generoPelicula.innerHTML += `
 				
                 
@@ -42,7 +23,6 @@ fetch ("https://api.themoviedb.org/3/movie/{movie_id}?api_key=15370bef1a25ea674d
 
 </ul>`
     }
->>>>>>> 4d58290fc3ec549c2db31b6eaec6303725c891e2
         
 })
 .catch (error => console.log(error))
@@ -50,7 +30,7 @@ fetch ("https://api.themoviedb.org/3/movie/{movie_id}?api_key=15370bef1a25ea674d
 
 let sectionseries = document.querySelector('.section-series')
 
-fetch (url)
+fetch ("https://api.themoviedb.org/3/movie/{movie_id}?api_key=15370bef1a25ea674deaaf70270ad202&language=en-US"
 .then(function(res) {
     return res.json();
 })
@@ -67,14 +47,9 @@ fetch (url)
 <a href="detail-genres.html?id=${generosDos[i].id}">${generosDos[i].name}</a>
 
 </li>
-<<<<<<< HEAD
-</ul>`    
-}
-=======
 
 </ul>`    
     }
->>>>>>> 4d58290fc3ec549c2db31b6eaec6303725c891e2
         
-
 })
+.catch(error => console.log(error))
