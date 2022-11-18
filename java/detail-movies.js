c1cba768f533579476303499bb4b54e020b68fe3
DelfinaPinasco <pinascod@udesa.edu.ar>
2022-11-18 15:57:01 -0300

    cambios

--- java/detail-movies.js
+++ java/detail-movies.js
@@ -5,13 +5,7 @@ window.addEventListener("load", function(){
 })
 
 let apikey = "15370bef1a25ea674deaaf70270ad202"
-<<<<<<< HEAD
-let url = `https://api.themoviedb.org/3/movie/{movie_id}?api_key=${apikey}&language=en-US
-
-`
-=======
 let url = `(https://api.themoviedb.org/3/movie/${movie_id}?${api_key}=15370bef1a25ea674deaaf70270ad202&language=en-US")`
->>>>>>> 8c0e607f9fa7b8b33495f2a7cce669e4419c6b70
 
 fetch (url)
     .then(function(res) {
@@ -20,11 +14,9 @@ fetch (url)
     .then(function(data) { 
     let info = data.results
     console.log(data);// hay que seguir con esto
-    
-
-        let container = document.querySelector('.') //aca va la class a la que queremos entrar
 
 
+    let contenido = document.querySelector('.contenedor') //aca va la class a la que queremos entrar
     contenido+=  `<img class = "fotofast" src= "https://image.tmdb.org/t/p/w500${info[index].poster_path}" alt='' /> 
     <h1>${info[index].title}</h1>
     <p class = "estreno"> Fecha de estreno: ${info[index].release_date} </p>
