b521f3c79a9a68948201e0f4e47b1ae78102f7d6
laramanfredini <laramanfredinii@gmail.com>
2022-11-18 09:38:30 -0300

    cambios

--- java/buscador.js
+++ java/buscador.js
@@ -3,23 +3,25 @@ let query2 = new URLSearchParams (query1)
 let search = query2.get("search")
 
 let results = this.document.querySelector(".results")
-results.innerHTML += `Resultados para: $(search)`
+results.innerHTML += `Resultados para: ${search}`
 
-fetch ("https://api.themoviedb.org/3/movie/{movie_id}?api_key=15370bef1a25ea674deaaf70270ad202&language=en-US")
+fetch (`https://api.themoviedb.org/3/search/movie?api_key=15370bef1a25ea674deaaf70270ad202&language=en-US&query=${search}&page=1&include_adult=false`)
 .then(function(res) {
     return res.json();
 })
 .then(function(data) {
     console.log(data);
     let searchResults = document.querySelector(".results");
-    let container = document.querySelector(".container_search")
+    let container = document.querySelector(".container")
     if (data.results.length == 0){
-        searchResults.innerHTML += `;a busqueda ${search} no dio ningun resultado`
+        searchResults.innerHTML += `La busqueda ${search} no dio ningun resultado`
     }
-    /* tengo que modificar un poco arriba y seguir aca
-    else
-    for
-     */
+  else{
+    for (let i=0; i<data.results.length; i++){
+
+    }
+}
+
 
 })
 .catch(function(error){
