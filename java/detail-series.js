2574956b87ed17de911b42aceda6e0c3d4169db7
nteller8 <nteller@udesa.edu.ar>
2022-11-19 00:28:08 -0300

    cambios quedo algo rari en el series maana lo arreglo

--- java/detail-series.js
+++ java/detail-series.js
@@ -1,22 +1,78 @@
-let query1 = location.search
-console.log (query1);
-let query2 = new URLSearchParams(query1)
-console.log(query2)
-let id = query2.get("id");
-console.log(id);
+/* 
+let seriesdetail = location.search;
+let seriesobjeto = new URLSearchParams(seriesdetail);
+let id = seriesobjeto.get('id');
+console.log(id)
 
-let apikey = "15370bef1a25ea674deaaf70270ad202"
-let url = "https://api.themoviedb.org/3/movie/{movie_id}?api_key=15370bef1a25ea674deaaf70270ad202&language=en-US"
+//let api_key = "15370bef1a25ea674deaaf70270ad202"
+let url1 = `https://api.themoviedb.org/3/tv/${id}?api_key=15370bef1a25ea674deaaf70270ad202&language=en-US`
 
-fetch (url)
-.then(function(res) {
-    return res.json();
+
+fetch(url1)
+.then(function (respuesta) {
+    return respuesta.json();
 })
-.then(function(data) {
+
+.then(function (data) {
     console.log(data);
-    // hay que seguir con esto
+    let container = document.querySelector('.seriespop')
+    let contenido = 
+        `
+    <article>
+    <h2>${data.name}</h2>
+    <img class = "fotofast" src= "https://image.tmdb.org/t/p/w500${data.poster_path}" alt='' /> 
+    <h1>${data.title}</h1>
+    <p class = "texto"> Fecha de estreno: ${data.release_date} </p>
+    <p class = "texto" > Calificación: ${data.vote_avarage} </p>
+    <p class = "texto" > Duración: ${data.runtime} </p>
+    <p class = "texto" > Calificación: ${data.vote_avarage} </p>
+    <p class = "texto" > Sinópsis: ${data.overview} </p>
+    </article>
+    `;
+    container.innerHTML += contenido
 })
-.catch(function(error){
-    console.log('El error es' + error);
-    return error
+
+
+    .catch(function (error) {
+        console.log(error);
+      
+})
+ */
+
+
+let seriedetail = location.search;
+let serieobjeto = new URLSearchParams(seriedetail);
+let id = serieobjeto.get('id');
+console.log(id)
+
+//let api_key = "15370bef1a25ea674deaaf70270ad202"
+let url = `https://api.themoviedb.org/3/tv/${id}?api_key=15370bef1a25ea674deaaf70270ad202&language=en-US`
+
+fetch(url)
+.then(function (respuesta) {
+    return respuesta.json();
+})
+
+.then(function (data) {
+    console.log(data);
+    let container = document.querySelector('.seriespop')
+    let contenido = 
+        `
+    <article>
+    <img class = "fotofast" src= "https://image.tmdb.org/t/p/w500${data.poster_path}" alt='' /> 
+    <h1>${data.title}</h1>
+    <p class = "texto"> Fecha de estreno: ${data.release_date} </p>
+    <p class = "texto" > Calificación: ${data.vote_avarage} </p>
+    <p class = "texto" > Duración: ${data.runtime} </p>
+    <p class = "texto" > Calificación: ${data.vote_avarage} </p>
+    <p class = "texto" > Sinópsis: ${data.overview} </p>
+    </article>
+    `;
+    container.innerHTML += contenido
+})
+
+
+    .catch(function (error) {
+        console.log(error);
+      
 })
