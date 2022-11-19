0c7f9894ab8f49b35c6b4e9de41b0d712a251c83
DelfinaPinasco <pinascod@udesa.edu.ar>
2022-11-19 17:10:25 -0300

    cambios

--- java/detail-genres.js
+++ java/detail-genres.js
@@ -1,2 +1,15 @@
+let detailgenre = window.location.search;
+let genres = new URLSearchParams(detailgenre);
+let id = genres.get('id');
+console.log(id)
 
+let url = `https://api.themoviedb.org/3/genre/movie/list?api_key=15370bef1a25ea674deaaf70270ad202&language=en-US`
 
+fetch (url)
+.then (function (respuesta) {
+    return respuesta.jason();
+}
+    
+
+    
+)
