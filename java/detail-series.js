abeeaabcdd6770d2086e32279f1dd53c9e98acea
nteller8 <nteller@udesa.edu.ar>
2022-11-19 17:17:45 -0300

    cambios

--- java/detail-series.js
+++ java/detail-series.js
@@ -7,6 +7,7 @@ console.log(id)
 let url = `https://api.themoviedb.org/3/tv/popular${id}?api_key=15370bef1a25ea674deaaf70270ad202&language=en-US&page=1`
 
 
+
 fetch(url)
     .then(function (respuesta) {
         return respuesta.json();
@@ -16,13 +17,12 @@ fetch(url)
         console.log(data);
         let container = document.querySelector('.contenedor')
         let contenido =
-        `<article class= "contenedor">
+        `<article class= "contenedor"> 
         <img class = "fotoriverdale" src="https://image.tmdb.org/t/p/w500${data.poster_path}" alt='' /> 
-        <a href ="./detail-serie.html?id=${data.id}">                 
-      
-         <h3>${data.name}</h3>
-         <h3>Fecha de estreno: ${data.first_air_date} </h3>
-         </a>
+        <h1>${data.name}</h1>
+        <p class = "texto"> Fecha de estreno: ${data.first_air_date} </p>
+        <p class = "texto" > Calificación: ${data.vote_average} </p>
+        <p class = "texto" > Sinópsis: ${data.overview} </p>
         </article>`;
         container.innerHTML += contenido
     })
