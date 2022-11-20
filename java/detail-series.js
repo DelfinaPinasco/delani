c43c962a50b59feba61988df001aafe7de5e0c2f
laramanfredini <laramanfredinii@gmail.com>
2022-11-20 11:31:59 -0300

    cambios

--- java/detail-series.js
+++ java/detail-series.js
@@ -4,8 +4,7 @@ let serieobjeto = new URLSearchParams(seriedetail);
 let id = serieobjeto.get('id');
 console.log(id)
 
-let url = `https://api.themoviedb.org/3/tv/{tv_id}?api_key=15370bef1a25ea674deaaf70270ad202&language=en-US`
-
+let url = `https://api.themoviedb.org/3/tv/${id}?api_key=15370bef1a25ea674deaaf70270ad202&language=en-US`
 
 
 fetch(url)
