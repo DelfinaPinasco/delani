75dc5be4409de53c5ded032f17d53f01ec73bbbb
nteller8 <nteller@udesa.edu.ar>
2022-11-20 16:27:08 -0300

    datos

--- java/search-results.js
+++ java/search-results.js
@@ -1,40 +1,46 @@
-let queryString = window.location.search;
-console.log(queryString);
-let queryObject = new URLSearchParams(queryString)
-let aBuscar = queryObject.get ('search');
-console.log(aBuscar);
+/* direccionarlo con el endpoint de searchresults,  esto es lo de rick & morty */
 
+let query = location.search; // recuperamos el querystring
+let stringToObject = new URLSearchParams(query); //La trasnformo en OL, // usando el querystring, creamos un objeto del tipo URLSearchParams
+let aBuscar = stringToObject.get('.search'); //Obtengo los datos de una propiedad con get()
+console.log(search);
 
 let apiKey = "15370bef1a25ea674deaaf70270ad202"
-let url = `https://api.themoviedb.org/3/search/multi?api_key=15370bef1a25ea674deaaf70270ad202&language=en-US&query=${aBuscar}%7D&page=1&include_adult=false`
+let url =`https://api.themoviedb.org/3/search/movie?api_key=15370bef1a25ea674deaaf70270ad202&language=en-US&query=${aBuscar}&page=1&include_adult=false`
 
-
-
-let resultados = document.querySelector('.resultados')
+let resultados = document.querySelector('.results')
 
 
 fetch(url)
-.then(function (respuestas){
-    return respuestas.json()
-})
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+        let info = data.results;
+        for (let i=0; i<info.length; i++){
+            if(info[i].media_type == "tv"){
+                
+                results.innerHTML += <li>
+                                            <a href="detail-series.html?id=${info[i].id}"">
+                                                <img src="https://image.tmdb.org/t/p/w500${info[i].poster_path}" alt="">
+                                             </a>
+                                        </li>
+                                            } else if (info[i].media_type == "movie"){
+                                        /* results.innerHTML += `<li>Pelicula: ${info[i].title}</li>` */
+                            
+                                        results.innerHTML += <li>
+                                                                    <a href="detail-movies.html?id=${info[i].id}">
+                                                                        <img src="https://image.tmdb.org/t/p/w500${info[i].poster_path}" alt="">
+                                                                    </a>
+                                                                </li>
+                                    }
+                                    
+                                }
+                                
+                            })
 
-.then(function(data) {
-    console.log(data);
-    let info = data.results;
+        
+    })
 
-    for (let i=0; i<info.length; i++){
-        if(info[i].media_type == "tv"){
-            /* resultados.innerHTML += `<li>Serie: ${info[i].original_name}</li>` */
-            resultados.innerHTML += `<li>
-            <img class = "fotofast" src= "https://image.tmdb.org/t/p/w500${data.poster_path}" alt='' /> 
-            <h1>${data.title}</h1>
-            </article>`
-    
-       
-        }
-    }
-})
-
-.catch(function(error){
-    console.log('El error es' + error);
-})
+.catch(error => console.log(error))
