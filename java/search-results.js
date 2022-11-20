dfed532a29c9a14a1bdac414c148cf68e5eb6945
nteller8 <nteller@udesa.edu.ar>
2022-11-20 12:06:30 -0300

    cambiosss

--- java/search-results.js
+++ java/search-results.js
@@ -6,41 +6,44 @@ let aBuscar = stringToObject.get('.search'); //Obtengo los datos de una propieda
 console.log(search);
 
 let apiKey = "15370bef1a25ea674deaaf70270ad202"
-let url =`https://api.themoviedb.org/3/search/movie?api_key=15370bef1a25ea674deaaf70270ad202&language=en-US&query=${aBuscar}&page=1&include_adult=false`
+let url = `https://api.themoviedb.org/3/search/multi?api_key=15370bef1a25ea674deaaf70270ad202&language=en-US&query=${aBuscar}%7D&page=1&include_adult=false`
+
+//`https://api.themoviedb.org/3/search/movie?api_key=15370bef1a25ea674deaaf70270ad202&language=en-US&query=${aBuscar}&page=1&include_adult=false`
+
 
 let resultados = document.querySelector('.results')
 
 
 fetch(url)
-    .then(function(response){
+    .then(function (response) {
         return response.json();
     })
-    .then(function(data){
+    .then(function (data) {
         console.log(data);
         let info = data.results;
-        for (let i=0; i<info.length; i++){
-            if(info[i].media_type == "tv"){
-                
+        for (let i = 0; i < info.length; i++) {
+            if (info[i].media_type == "tv") {
+
                 results.innerHTML += <li>
-                                            <a href="detail-series.html?id=${info[i].id}"">
-                                                <img src="https://image.tmdb.org/t/p/w500${info[i].poster_path}" alt="">
-                                             </a>
-                                        </li>
-                                            } else if (info[i].media_type == "movie"){
-                                        /* results.innerHTML += `<li>Pelicula: ${info[i].title}</li>` */
-                            
-                                        results.innerHTML += <li>
-                                                                    <a href="detail-movies.html?id=${info[i].id}">
-                                                                        <img src="https://image.tmdb.org/t/p/w500${info[i].poster_path}" alt="">
-                                                                    </a>
-                                                                </li>
-                                    }
-                                    
-                                }
-                                
-                            })
+                    <a href="detail-series.html?id=${info[i].id}"">
+                    <img src="https://image.tmdb.org/t/p/w500${info[i].poster_path}" alt="">
+                    </a>
+                </li>
+            } else if (info[i].media_type == "movie") {
+                /* results.innerHTML += `<li>Pelicula: ${info[i].title}</li>` */
+
+                results.innerHTML += <li>
+                    <a href="detail-movies.html?id=${info[i].id}">
+                        <img src="https://image.tmdb.org/t/p/w500${info[i].poster_path}" alt="">
+                    </a>
+                </li>
+            }
+
+        }
+
+    })
 
         
     })
 
-.catch(error => console.log(error))
+.catch (error => console.log(error))
