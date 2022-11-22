d8dbfffd90c2d68db1f515c526d583fdd330b9d7
nteller8 <nteller@udesa.edu.ar>
2022-11-22 12:06:01 -0300

    cambios

--- java/detail-genres.js
+++ java/detail-genres.js
@@ -1,77 +1,76 @@
-let detailgenre = window.location.search;
-console.log (detailgenre);
-let genres = new URLSearchParams(detailgenre);
-let id = genres.get('id');
-console.log(id);
-let genero = "movie"
+
+let queryString = location.search 
+let queryStringToObject = new URLSearchParams(queryString); 
+let id = queryStringToObject.get('id');
 
 //peliculas
-let priurl = `https://api.themoviedb.org/3/discover/movie?api_key=15370bef1a25ea674deaaf70270ad202&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`
-fetch(priurl)
-    .then(function (respuesta) {
-    return respuesta.json();
-    })
-    .then(function (data) {
+let url = `https://api.themoviedb.org/3/discover/movie?api_key=15370bef1a25ea674deaaf70270ad202&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`
+    fetch(url)
+    .then(function(response){
+        return response.json()
+
+        })
+
+    .then (function(data){
         console.log(data);
-        let pelis = document.querySelector(".peliculas") //nuestra variable
-        let info = data.results
-        console.log(info);
-       // let contenido = "";
-        //console.log (info)
-
-        for (let index = 0; index < info.length; index++) {         /* i++ se va iterando */ 
-        pelis.innerHTML +=
-        `<article class= "container img">
-        <a href ="./detail-movie.html?id=${info[index].id}">                 
-        <img src="https://image.tmdb.org/t/p/w500${info[index].poster_path}" alt='' />
-         <h3>${info[index].title}</h3>
-         </a>
-        </article>`
-          
-    }
-    //pelis.innerHTML += contenido
-
-    })
-
-    .catch(function (error) {
-        console.log('El error es' + error);
-    })
-
-
-    //series
-    let genero2 = "tv"
-    let segurl = `https://api.themoviedb.org/3/discover/tv?api_key=15370bef1a25ea674deaaf70270ad202&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=0`
-fetch(segurl)
-.then(function (respuesta) {
-    return respuesta.json();
-})
-.then(function (data) {
-    console.log(data);
-    let info = data.results ;
-    let series = document.querySelector(".series") //nuestra variable
-    let contenido = "";
-
-    for (let index = 0; index < info.length; index++) {         /* i++ se va iterando */ 
-        contenido+=
-    `<article class= "container img">
-     <a href ="./detail-serie.html?id=${info[index].id}">    
-     <img src="https://image.tmdb.org/t/p/w500${info[index].poster_path}" alt='' />
-      <h3>${info[index].name}</h3>
-      </a>
-     </article>`    
-
+        let generoApi = data.results
+        let bContainer= document.querySelector('.sectionnn');
+        let contenidoT= ''
+        for (let i =0 ; i<5; i++){
+            contenidoT +=
+            `<article class="peliculas">
+            <a href="./detalle-genres.html?id=${generoApi[i].id}">
+            <div class="bloque-item-lista">
+                <h1 id="tituloGenero" class="titulopelis">${generoApi[i].original_title}</h1>
+                <img id="imagenPelicula" class="imagenes" src="https://image.tmdb.org/t/p/w500${generoApi[i].poster_path}" alt="foto1">
+                <p id="textoPelicula" class="texto"> ${generoApi[i].overview}</p>
+                <p class="estreno">${generoApi[i].release_date} </p>
+            </div>
+             </a>
+            </article>`
+            
+        }
+        console.log(contenidoT);
+        bContainer.innerHTML = contenidoT
     
-    }
-
-    series.innerHTML += contenido
-
     })
+        
+     
+    
 
-    .catch(function (error) {
-        console.log('El error es' + error);
+//series
+
+let urlGeneros = `https://api.themoviedb.org/3/discover/tv?api_key=15370bef1a25ea674deaaf70270ad202&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=0`
+    fetch(urlGeneros)
+    .then(function(response){
+        return response.json()
+
+        })
+
+    .then (function(data){
+        console.log(data);
+        let generoApi = data.results
+        let bContainer= document.querySelector('.sectionnn');
+        let contenidoT= ''
+        for (let i =0 ; i<5; i++){
+            contenidoT +=
+            `<article class="series">
+            <a href="detalle-genres.html?id=${generoApi[i].id}">
+            <div class="bloque-item-lista">
+                <h1 id="tituloGenero" class="titulopelis">${generoApi[i].original_title}</h1>
+                <img id="imagenPelicula" class="imagenes" src="https://image.tmdb.org/t/p/w500${generoApi[i].poster_path}" alt="foto1">
+                <p id="textoPelicula" class="texto"> ${generoApi[i].overview}</p>
+                <p class="estreno">${generoApi[i].release_date} </p>
+            </div>
+             </a>
+            </article>`
+            
+        }
+        console.log(contenidoT);
+        bContainer.innerHTML = contenidoT
+    
     })
+        
 
 
-
-
-
+ 
