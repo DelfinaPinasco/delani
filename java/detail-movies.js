d0ef039120b424365155458eaa6042450ce1f989
nteller8 <nteller@udesa.edu.ar>
2022-11-22 01:46:41 -0300

    cambios

--- java/detail-movies.js
+++ java/detail-movies.js
@@ -35,30 +35,46 @@ fetch(url)
     })
 
 
-    //Boton de favoritos
+    // FAVS
+let favoritos = [];
 
-    let favoritosm = []
+let recuperoStorage = localStorage.getItem('peliculas_favoritas'); // te va a devolver null o los datos
 
-    let recuperoStorage = localStorage.getItem("favoritos")
+if (recuperoStorage != null) {
+    //1ero tenemos que transformarlo de cadena de texto con JSON.parse y despues lo guardamos en favoritos 
+    favoritos = JSON.parse(recuperoStorage);
+}
 
-    if (recuperoStorage != null) {
-        favoritosm = JSON.parse(recuperoStorage)
+// Hacer click en el link. Primero deberemos capturar el elemento
+let fav = document.querySelector('.boton2');
+
+// Chequear que id este en el array de favoritos 
+if (favoritos.includes(id)) {
+    fav.innerText = "Quitar de favoritos"
+}
+
+fav.addEventListener('click', function (evento) {
+    evento.preventDefault();
+
+    if (favoritos.includes(id)) {
+        // Si el id esta en el array
+        let indice = favoritos.indexOf(id);
+
+        //Borrar a partir del indice 1 elemento 
+        favoritos.splice(indice, 1)
+        fav.innerText = "Agregar a favoritos"
     }
 
-    if (favoritosm.includes(pelicula)) {
-        favoritosm.innerText = "Quitar de favoritos";
+    else { // Guardar dato en un array: agregar un dato al array 
+        favoritos.push(id);
+        fav.innerText = "Quitar de favoritos";
     }
-    fav.addEventListener ("click", function(e){
-        e.preventDefault();
-        if (favoritosm.includes(pelicula)){
-            let indice = favoritosm.indexOf(pelicula)
-            favoritosm.splice(indice,1);
-            fav.innerText = "Agregar a favoritos";
 
-        }else{
-            favoritosm.push(pelicula)
-            fav.innerText = "Quitar de favoritos"
-        }
-        let favsToString = JSON.stringify(favoritosm);
-        localStorage.setItem ("favoritos", favsToString)
-    })
+    // Guardar el array en el storage (esto se hace pase lo que pase, no se mete en el else)
+    let favsToString = JSON.stringify(favoritos); // Transformamos el array en una cadena de texto
+
+    localStorage.setItem("peliculas_favoritas", favsToString);
+
+    console.log(localStorage);
+
+})
